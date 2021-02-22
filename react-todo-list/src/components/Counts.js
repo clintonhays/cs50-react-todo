ac7e3983c3763367fc0bd9205597c873356113d5
clintonhays <clintonhays84@gmail.com>
2021-02-22 21:21:34 +0900

    add local storage for todos

--- react-todo-list/src/components/Counts.js
+++ react-todo-list/src/components/Counts.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 const Counts = ({ todos }) => {
-  return (
-    <div className="countContainer">
-      <div className="totalTodos">
-        <h2>Tasks: {todos.length}</h2>
-      </div>
-      <div className="unfinishedTodos">
-        <h2>
-          Unfinished:
-          {todos.filter((todo) => todo.checked === false).length}
-        </h2>
-      </div>
-    </div>
-  );
+	return (
+		<div className="countContainer">
+			<div className="totalTodos">
+				<h2>Tasks: {todos.length}</h2>
+			</div>
+			<div className="unfinishedTodos">
+				<h2>
+					Unfinished:{` `}
+					{todos.filter((todo) => todo.checked === false).length}
+				</h2>
+			</div>
+		</div>
+	);
 };
 
 export default Counts;
