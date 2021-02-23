ae09f0714e10e82dbd24fcc1aab05defc6f7ac84
clintonhays <clintonhays84@gmail.com>
2021-02-23 15:27:33 +0900

    document AddForm.js

--- react-todo-list/src/components/AddForm.js
+++ react-todo-list/src/components/AddForm.js
@@ -1,28 +1,43 @@
 import React, { useRef } from "react";
 
-const AddForm = (props) => {
-  const todoInput = useRef("");
+const AddForm = ({ props }) => {
+	// create reference to input
+	const todoInput = useRef("");
 
-  const addTodo = (e) => {
-    props.addTodo(todoInput.current.value);
-    todoInput.current.value = null;
-    todoInput.current.focus();
-    e.preventDefault();
-  };
+	/**
+	 * Sends input value to addTodo() in App,js
+	 * @param {str} e keystroke in the input field
+	 */
+	const addTodo = (e) => {
+		// callback to add text parameter to addTodo() in App.js
+		props.addTodo(todoInput.current.value);
+		// reset input field to empty
+		todoInput.current.value = null;
+		// refocus to input field after submission
+		todoInput.current.focus();
+		// prevent submit from refreshing the page
+		e.preventDefault();
+	};
 
-  return (
-    <div className="formWrapper">
-      <form onSubmit={addTodo}>
-        <label className="inputLabel" htmlFor="todoInput">
-          What would you like to do?
-        </label>
-        <input type="text" name="todoInput" id="todoInput" placeholder="add your task" ref={todoInput} />
-        <button className="addTodo" onClick={addTodo}>
-          Add Todo
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="formWrapper">
+			<form onSubmit={addTodo}>
+				<label className="inputLabel" htmlFor="todoInput">
+					What would you like to do?
+				</label>
+				<input
+					type="text"
+					name="todoInput"
+					id="todoInput"
+					placeholder="add your task"
+					ref={todoInput}
+				/>
+				<button className="addTodo" onClick={addTodo}>
+					Add Todo
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default AddForm;
