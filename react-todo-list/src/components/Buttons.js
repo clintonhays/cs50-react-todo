cc244329a38bfa60a7e7e3f7b5fff30a089b74c0
clintonhays <clintonhays84@gmail.com>
2021-02-23 15:33:00 +0900

    document Buttons.js

--- react-todo-list/src/components/Buttons.js
+++ react-todo-list/src/components/Buttons.js
@@ -1,37 +1,50 @@
 import React, { useState } from "react";
 
 const Buttons = (props) => {
-  const [complete, setComplete] = useState(false);
-  // const
+	// set initial state and updater
+	const [complete, setComplete] = useState(false);
 
-  const toggleComplete = () => {
-    setComplete(!complete);
-  };
+	/**
+	 * Toggles completed state
+	 */
+	const toggleComplete = () => {
+		setComplete(!complete);
+	};
 
-  const toggleTodo = () => {
-    props.toggleTodo(props.id);
-  };
+	/**
+	 * Feeds todo id to toggleTodo() callback in App.js
+	 */
+	const toggleTodo = () => {
+		props.toggleTodo(props.id);
+	};
 
-  const deleteTodo = () => {
-    props.deleteTodo(props.id);
-  };
+	/**
+	 * Feeds todo id to deleteTodo() callback in App.js
+	 */
+	const deleteTodo = () => {
+		props.deleteTodo(props.id);
+	};
 
-  return (
-    <div className="todoButtons">
-      <button
-        className="completeButton"
-        onClick={() => {
-          toggleTodo();
-          toggleComplete();
-        }}
-      >
-        {complete ? <i className="lni lni-checkmark-circle"></i> : <i className="lni lni-spiner-solid"></i>}
-      </button>
-      <button className="deleteButton" onClick={deleteTodo}>
-        <i className="lni lni-trash"></i>
-      </button>
-    </div>
-  );
+	return (
+		<div className="todoButtons">
+			<button
+				className="completeButton"
+				onClick={() => {
+					toggleTodo();
+					toggleComplete();
+				}}
+			>
+				{complete ? (
+					<i className="lni lni-checkmark-circle"></i>
+				) : (
+					<i className="lni lni-spiner-solid"></i>
+				)}
+			</button>
+			<button className="deleteButton" onClick={deleteTodo}>
+				<i className="lni lni-trash"></i>
+			</button>
+		</div>
+	);
 };
 
 export default Buttons;
